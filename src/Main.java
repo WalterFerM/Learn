a22edab72cc57c0fe9676d777c2f1cd5de37b5c8
WalterFerM <wffmen@gmail.com>
2022-05-12 17:45:05 -0500

    Leccion 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        suma(10,20, 5);
+        suma(10, 5, 20);
+
+        Car miCar = new Car();
+
+        miCar.sumDoors();
+        miCar.sumDoors();
+        System.out.println("The number of Doors is :"+ miCar.numbDoors);
+
     }
 
     public static void suma (int a, int b, int c){
-        System.out.println(a+b+c);
+        int solution;
+        solution = a+b+c;
+        System.out.println(solution);
+
     }
 
+
+
 }
+class Car {
+    public int numbDoors=4;
+    public void sumDoors (){
+        this.numbDoors++;
+    }
+}
+
